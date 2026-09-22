8ebb48ec469992cee091ce496968dacfc9cfa9ee
_EliteNightmare <71969778+EliteNightmare@users.noreply.github.com>
2026-09-22 14:53:39 +0200

    Create TopBar.tsx

---
+++ src/react-app/components/TopBar.tsx
@@ -0,0 +1,27 @@
+import "./TopBar.css";
+
+import logo from "../assets/discordinylogo.png";
+import discordIcon from "../assets/discord_icon.png";
+
+export default function TopBar() {
+  return (
+    <header className="top-bar">
+      <div className="top-bar-left">
+        <button className="logo-button" type="button">
+          <img src={logo} alt="Discordiny" className="discordiny-logo" />
+        </button>
+
+        <nav className="main-navigation">
+          <button type="button">Home</button>
+          <button type="button">World</button>
+          <button type="button">Inventory</button>
+        </nav>
+      </div>
+
+      <button className="account-button" type="button">
+        <img src={discordIcon} alt="Discord account" />
+        <span>Account</span>
+      </button>
+    </header>
+  );
+}
