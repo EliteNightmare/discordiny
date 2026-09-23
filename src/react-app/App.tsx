3f30d61ffc47a881c253f00e5d7787312bd816ea
_EliteNightmare <71969778+EliteNightmare@users.noreply.github.com>
2026-09-23 12:23:11 +0200

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,11 +1,16 @@
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Account from "./pages/Account";
 
 function App() {
   const path = window.location.pathname;
 
   if (path === "/profile") {
     return <Profile />;
+  }
+
+  if (path === "/account") {
+    return <Account />;
   }
 
   return <Home />;
