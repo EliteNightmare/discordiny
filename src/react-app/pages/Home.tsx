6fe06530858ac74cb926a226ac77caeaad2cb46e
_EliteNightmare <71969778+EliteNightmare@users.noreply.github.com>
2026-09-23 12:11:38 +0200

    Update Home.tsx

--- src/react-app/pages/Home.tsx
+++ src/react-app/pages/Home.tsx
@@ -4,7 +4,7 @@ import TopBar from "../components/TopBar";
 
 export default function Home() {
   function startDiscordLogin() {
-    window.location.href = "/api/auth/login";
+    window.location.href = "/profile";
   }
 
   return (
