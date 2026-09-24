5deea62e7fec9f892a1e0577fd5e936165da1cd5
_EliteNightmare <71969778+EliteNightmare@users.noreply.github.com>
2026-09-24 11:35:13 +0200

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Account from "./pages/Account";
+import Activities from "./pages/Activities";
 
 function App() {
   const path = window.location.pathname;
@@ -11,6 +12,10 @@ function App() {
 
   if (path === "/account") {
     return <Account />;
+  }
+
+  if (path === "/activities") {
+    return <Activities />;
   }
 
   return <Home />;
