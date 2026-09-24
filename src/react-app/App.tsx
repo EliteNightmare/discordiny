f4edad0c1d3318cc0c5f0ea32f4c6717900f7ea0
_EliteNightmare <71969778+EliteNightmare@users.noreply.github.com>
2026-09-25 00:51:24 +0200

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -6,7 +6,22 @@ import Vault from "./pages/Vault";
 import VaultCategory from "./pages/VaultCategory";
 import WeaponVault from "./pages/WeaponVault";
 
+import SiteGate, {
+  shouldGateSite,
+} from "./components/SiteGate";
+
 function App() {
+  /*
+   * GLOBAL SITE OVERRIDE
+   *
+   * If Discordiny is in maintenance
+   * or countdown mode, nothing below
+   * this point gets rendered.
+   */
+  if (shouldGateSite()) {
+    return <SiteGate />;
+  }
+
   const path = window.location.pathname;
 
   if (path === "/profile") {
@@ -29,7 +44,7 @@ function App() {
     path.match(
       /^\/vault\/([^/]+)\/([^/]+)\/?$/,
     );
-  
+
   if (weaponVaultMatch) {
     return (
       <WeaponVault
@@ -44,12 +59,16 @@ function App() {
   }
 
   const vaultCategoryMatch =
-    path.match(/^\/vault\/([^/]+)\/?$/);
-  
+    path.match(
+      /^\/vault\/([^/]+)\/?$/
+    );
+
   if (vaultCategoryMatch) {
     return (
       <VaultCategory
-        categorySlug={vaultCategoryMatch[1]}
+        categorySlug={
+          vaultCategoryMatch[1]
+        }
       />
     );
   }
