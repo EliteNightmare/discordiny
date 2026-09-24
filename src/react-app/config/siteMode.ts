193f5c258573241612802991df36257e0ada75d8
_EliteNightmare <71969778+EliteNightmare@users.noreply.github.com>
2026-09-25 00:49:28 +0200

    Create siteMode.ts

---
+++ src/react-app/config/siteMode.ts
@@ -0,0 +1,24 @@
+export type SiteMode =
+  | "live"
+  | "maintenance"
+  | "countdown";
+
+export const SITE_MODE: SiteMode =
+  "live";
+
+/*
+ * Only used when SITE_MODE is "countdown".
+ *
+ * IMPORTANT:
+ * Include the timezone offset so there is no ambiguity.
+ *
+ * Examples:
+ *
+ * Belgium summer time:
+ * "2026-10-01T20:00:00+02:00"
+ *
+ * Belgium winter time:
+ * "2026-12-01T20:00:00+01:00"
+ */
+export const COUNTDOWN_TARGET =
+  "2026-10-01T20:00:00+02:00";
